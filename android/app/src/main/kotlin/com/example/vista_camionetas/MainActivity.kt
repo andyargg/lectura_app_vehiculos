120c965058ea9df689ecd923432e256fb13d3e86
andyargg <andresarguin04@gmail.com>
2025-03-13 00:59:42 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/vista_camionetas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vista_camionetas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Pages/data_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DataPage extends StatefulWidget {
+  const DataPage({super.key});
+
+  @override
+  State<DataPage> createState() => _DataPageState();
+}
+
+class _DataPageState extends State<DataPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
